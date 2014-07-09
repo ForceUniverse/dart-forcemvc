872e09409177c51d9909ae827a6b1a127890b96a
joris hermans <JHermans85@hotmail.com>
2014-07-09 21:58:22 +0200

    review template

--- lib/render/force_mustache_render.dart
+++ lib/render/force_mustache_render.dart
@@ -6,8 +6,13 @@ class MustacheRender extends ForceViewRender {
   MustacheRender([views = "../views/", clientFiles = "../build/web/", clientServe = true]) : super(views, clientFiles, clientServe);
   
   String _render_impl(String template, model) {
-    var output = render(template, model, delimiter: delimiter);
+    var output = render(_reviewTemplate(template), model, delimiter: delimiter);
     return _transform(output); 
+  }
+  
+  String _reviewTemplate(String result) {
+    result = result.replaceAll("${delimiter.opening}&amp;", "${delimiter.opening}&");
+    return result;
   }
   
   String _transform(String result) {
@@ -19,7 +24,6 @@ class MustacheRender extends ForceViewRender {
     result = result.replaceAll("src='/../", "src='../");
     result = result.replaceAll("src='//", "src='/");
     result = result.replaceAll("src=\"//", "src=\"/");
-    result = result.replaceAll("${delimiter.opening}&amp;", "${delimiter.opening}&");
     return result;
   }
 }
