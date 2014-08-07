276084a82cdf470b6ccdea3ba85fc189e5f89a24
joris hermans <JHermans85@hotmail.com>
2014-08-07 22:10:03 +0200

    also change Authentication annotation!

--- lib/annotations/metadata.dart
+++ lib/annotations/metadata.dart
@@ -4,9 +4,11 @@ part of dart_force_mvc_lib;
  * Annotation that will be used when the methods of the class needing authentication
  *
  */
-class Authentication {
+const Authentication = const _Authentication();
+
+class _Authentication {
   
-  const Authentication();
+  const _Authentication();
   
 }
 
