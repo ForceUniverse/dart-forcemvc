762dc715d8cb456a257f750f1b4ad8b9d9b11d9b
luis <luisbigvargas@gmail.com>
2014-08-16 19:22:36 -0400

    adding HttpRequest

--- lib/security/no_security_strategy.dart
+++ lib/security/no_security_strategy.dart
@@ -2,7 +2,7 @@ part of dart_force_mvc_lib;
 
 class NoSecurityStrategy extends SecurityStrategy<dynamic> {
   
-  bool checkAuthorization(obj, List<String> roles, data) => true;   
+  bool checkAuthorization(HttpRequest obj, List<String> roles, data) => true;   
   
   Uri getRedirectUri(HttpRequest req) {
     return Uri.parse("/");
