fc1ceef8de56f96ad815e363385e712b315e9cb9
luis <luisbigvargas@gmail.com>
2014-08-16 16:08:51 -0400

    Reformat Code

--- lib/error/handler_exception_resolver.dart
+++ lib/error/handler_exception_resolver.dart
@@ -7,24 +7,25 @@ part of dart_force_mvc_lib;
  */
 
 abstract class HandlerExceptionResolver {
-  
-    /**
-     * Try to resolve the given exception that got thrown during on handler execution,
-     * returning a String that the viewname or return null if you want to return a json representation.
-     * @param request current Force Request, an encapsulated HttpRequest
-     * @param ex the exception that got thrown during handler execution
-     * @return a corresponding String that represents the viewname of your template
-     */
-  String resolveException(ForceRequest request, Model model, Exception ex);
-  
+
   /**
-       * Try to resolve the given exception that got thrown during on handler error, 
-       * an error is different from an exception, it is an error that happens on runtime.
-       * returning a String that the viewname or return null if you want to return a json representation.
-       * @param request current Force Request, an encapsulated HttpRequest
-       * @param ex the exception that got thrown during handler execution
-       * @return a corresponding String that represents the viewname of your template
-       */
-    String resolveError(ForceRequest request, Model model, Error er);
-  
+   * Try to resolve the given exception that got thrown during on handler execution,
+   * returning a String that the viewname or return null if you want to return a json representation.
+   * @param request current Force Request, an encapsulated HttpRequest
+   * @param ex the exception that got thrown during handler execution
+   * @return a corresponding String that represents the viewname of your template
+   */
+  String resolveException(ForceRequest request, Model model, Exception ex);
+
+  /**
+   * Try to resolve the given exception that got thrown during on handler error,
+   * an error is different from an exception, it is an error that happens on runtime.
+   * returning a String that the viewname or return null if you want to return a json representation.
+   * @param request current Force Request, an encapsulated HttpRequest
+   * @param ex the exception that got thrown during handler execution
+   * @return a corresponding String that represents the viewname of your template
+   */
+
+  String resolveError(ForceRequest request, Model model, Error er);
+
 }
