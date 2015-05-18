097b6c5362d6eae289e03fb91c0c70382e9867c6
joris hermans <JHermans85@hotmail.com>
2015-05-18 20:30:22 +0200

    update tests to newer package

--- test/locale_resolver_test.dart
+++ test/locale_resolver_test.dart
@@ -1,4 +1,4 @@
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'package:forcemvc/force_mvc.dart';
 
 main() {  
@@ -7,10 +7,13 @@ main() {
   
   var accept_header_string = "en-ca,en;q=0.8,en-us;q=0.6,de-de;q=0.4,de;q=0.2";
   
-  AcceptHeaderLocaleResolver lr = new AcceptHeaderLocaleResolver();
+  test("test locale resolving", () {
+      AcceptHeaderLocaleResolver lr = new AcceptHeaderLocaleResolver();
   
-  lr.resolveLocaleWithHeader(accept_header_string);
-  
+      Locale locale = lr.resolveLocaleWithHeader(accept_header_string);
+      
+      expect("en_CA", locale.toString());
+  });
   
 }
 
--- test/locale_test.dart
+++ test/locale_test.dart
@@ -1,4 +1,4 @@
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'package:forcemvc/force_mvc.dart';
 
 main() {  
