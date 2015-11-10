525a3f03adebfe0ddd767241af8fb437c68bc005
joris hermans <JHermans85@hotmail.com>
2015-11-10 20:31:44 +0100

    optimize code throw away unused code

--- test/locale_resolver_test.dart
+++ test/locale_resolver_test.dart
@@ -1,19 +1,16 @@
 import 'package:test/test.dart';
 import 'package:forcemvc/force_mvc.dart';
 
-main() {  
+main() {
   // First tests!  
-  var locale = Locale.ENGLISH;
-  
   var accept_header_string = "en-ca,en;q=0.8,en-us;q=0.6,de-de;q=0.4,de;q=0.2";
-  
+
   test("test locale resolving", () {
       AcceptHeaderLocaleResolver lr = new AcceptHeaderLocaleResolver();
-  
+
       Locale locale = lr.resolveLocaleWithHeader(accept_header_string);
-      
+
       expect("en_CA", locale.toString());
   });
-  
-}
 
+}
