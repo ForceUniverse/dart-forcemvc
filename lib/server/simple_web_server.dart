90ffbbbbd50b9284d476f45841f16eee3e2c0f57
joris hermans <joris.hermans@goudengids.be>
2015-12-29 12:49:39 +0100

    Solve an issue with HttpServer.bindSecure

--- lib/server/simple_web_server.dart
+++ lib/server/simple_web_server.dart
@@ -2,9 +2,9 @@ part of dart_force_mvc_lib;
 
 class SimpleWebServer {
   final Logger log = new Logger('SimpleServer');
-  
+
   Router router;
-  
+
   var host;
   var port;
   var wsPath;
@@ -13,10 +13,10 @@ class SimpleWebServer {
   var clientServe;
   var virDir;
   var bind_address = InternetAddress.ANY_IP_V6;
-  
+
   Completer _completer = new Completer.sync();
 
-  SimpleWebServer(this.host,    
+  SimpleWebServer(this.host,
                   this.port,
                   this.wsPath,
                   this.staticFiles,
@@ -24,19 +24,19 @@ class SimpleWebServer {
                   this.clientServe) {
     init();
   }
-  
+
   void init() {
     if (host != null) {
       this.bind_address = host;
     }
-    
+
     if(clientServe == true) {
        String clientFilesPath = Platform.script.resolve(clientFiles).toFilePath();
-               
+
        _exists(clientFilesPath);
     }
   }
-     
+
   void _exists(dir) {
      try {
        if (!new Directory(dir).existsSync()) {
@@ -46,15 +46,15 @@ class SimpleWebServer {
        log.severe("The '$dir' directory was not found.");
      }
   }
-  
+
   /**
      * This method helps to start your webserver.
-     * 
+     *
      * You can add a [WebSocketHandler].
      */
-  
+
   Future start({WebSocketHandler handleWs: null, FallbackStart fallback}) {
-    HttpServer.bind(bind_address, port).then((server) { 
+    HttpServer.bind(bind_address, port).then((server) {
       _onStartComplete(server, handleWs);
     }).catchError((e) {
       if (fallback==null) {
@@ -64,45 +64,44 @@ class SimpleWebServer {
         fallback(this, handleWs);
       }
     });
-    
+
     return _completer.future;
   }
-  
+
   /**
      * This method helps to start your webserver in a secure way.
-     * 
-     * You can add a [WebSocketHandler], certificateName,
-     * 
+     *
+     * You can add a [WebSocketHandler], [SecurityContext], requestClientCertificate, backlog
+     *
      * The optional argument [backlog] can be used to specify the listen
      * backlog for the underlying OS listen setup. If [backlog] has the
      * value of [:0:] (the default) a reasonable value will be chosen by
      * the system.
      *
-     * The certificate with nickname or distinguished name (DN) [certificateName]
-     * is looked up in the certificate database, and is used as the server
-     * certificate. If [requestClientCertificate] is true, the server will
-     * request clients to authenticate with a client certificate. 
+     * The certificate chain and the private key are set in the [context] SecurityContext object
+     * that is passed to bindSecure.
+     *
      */
-  
-  Future startSecure({WebSocketHandler handleWs: null, String certificateName, bool requestClientCertificate: false,
+
+  Future startSecure({WebSocketHandler handleWs: null, SecurityContext context, bool requestClientCertificate: false,
     int backlog: 0}) {
-      HttpServer.bindSecure(bind_address, port, certificateName: certificateName,
+      HttpServer.bindSecure(bind_address, port, context,
           requestClientCertificate: requestClientCertificate,
-          backlog: backlog).then((server) { 
+          backlog: backlog).then((server) {
         _onStartComplete(server, handleWs);
       }).catchError((e) {
         var error = _errorOnStart(e);
         return new Future.error(error);
       });
-      
+
       return _completer.future;
     }
-  
+
   void _onStartComplete(Stream<HttpRequest> incoming, [WebSocketHandler handleWs]) {
     _onStart(incoming, handleWs);
     _completer.complete(const []);
   }
-  
+
   Error _errorOnStart(e) {
     log.warning("Could not startup the web server ... $e");
     log.warning("Is your port already in use?");
@@ -112,7 +111,7 @@ class SimpleWebServer {
   Stream<HttpRequest> serve(String name) {
     return router.serve(name);
   }
-  
+
   void setupConsoleLog([Level level = Level.INFO]) {
       Logger.root.level = level;
       Logger.root.onRecord.listen((LogRecord rec) {
@@ -124,7 +123,7 @@ class SimpleWebServer {
         }
       });
   }
- 
+
   _onStart(Stream<HttpRequest> incoming, [WebSocketHandler handleWs]) {}
 }
 
@@ -150,7 +149,7 @@ typedef FallbackStart(SimpleWebServer sws, WebSocketHandler wsHandler);
 var randomPortFallback = (SimpleWebServer sws, WebSocketHandler wsHandler) {
   var rng = new Random();
   var newPortNumber = rng.nextInt(8888) + 1000;
-  
+
   sws.port = newPortNumber;
   sws.start(handleWs: wsHandler);
 };
--- lib/test.dart
+++ lib/test.dart
@@ -9,22 +9,22 @@ import 'dart:convert';
 class MockHttpRequest extends Mock implements HttpRequest {}
 
 class MockForceRequest implements ForceRequest {
-  
+
    var postData;
    Map<String, String> postParams = new Map<String, String>();
    List<Cookie> mockCookies = new List<Cookie>();
-   
+
    HttpRequest request;
    Map<String, String> path_variables;
    Completer _asyncCallCompleter;
    Locale locale;
-   
+
    MockForceRequest({this.postData: "test"}) {
-     path_variables = new Map<String, String>(); 
+     path_variables = new Map<String, String>();
      _asyncCallCompleter = new Completer();
      request = new MockHttpRequest();
    }
-   
+
    List header(String name) => new List();
 
    bool accepts(String type) => true;
@@ -34,6 +34,8 @@ class MockForceRequest implements ForceRequest {
    bool get isForwarded => false;
 
    List<Cookie> get cookies => mockCookies;
+
+   void statusCode(int statusCode) {}
    
    Future<dynamic> getPostData({ bool usejson: true }) {
      Completer<dynamic> completer = new Completer<dynamic>();
@@ -41,23 +43,23 @@ class MockForceRequest implements ForceRequest {
 
      return completer.future;
    }
-   
+
    Future<Map<String, String>> getPostRawData() {
        Completer c = new Completer();
        c.complete(postParams);
        return c.future;
      }
-   
+
    Future<Map<String, String>> getPostParams({ Encoding enc: UTF8 }) {
      Completer c = new Completer();
      c.complete(postParams);
      return c.future;
    }
-   
+
    void async(value) {
      this._asyncCallCompleter.complete(value);
    }
-   
+
    Future get asyncFuture => _asyncCallCompleter.future;
-  
+
 }
