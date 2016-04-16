053a5933bccbecbd18629663481abb66c28a9cdc
joris hermans <jhermans85@hotmail.com>
2016-04-16 20:54:59 +0200

    refactor http message converter

--- lib/converters/json_http_message_converter.dart
+++ lib/converters/json_http_message_converter.dart
@@ -1,20 +1,16 @@
 part of dart_force_mvc_lib;
 
-class JsonHttpMessageConverter<T> extends HttpMessageConverter<T> {
+class JsonHttpMessageConverter<T> extends AbstractHttpMessageConverter<T> {
 
-  canRead(MediaType mediaType) {
-    return mediaType.hasSame(MediaType.APPLICATION_JSON);
-  }
-
-  bool canWrite(MediaType mediaType) {
-    return mediaType.hasSame(MediaType.APPLICATION_JSON);
+  JsonHttpMessageConverter() {
+    setSupportedMediaTypes([MediaType.APPLICATION_JSON]);
   }
 
   List<MediaType>	getSupportedMediaTypes() { return [MediaType.APPLICATION_JSON]; }
 
-  T	read(HttpInputMessage inputMessage) { return null; }
+  T	readInternal(HttpInputMessage inputMessage) { return null; }
 
-  void	write(T t, MediaType contentType, HttpOutputMessage outputMessage) {
+  void	writeInternal(T t, HttpOutputMessage outputMessage) {
       // write things to the response ... outputMessage.getBody().
       String data = JSON.encode(t);
       outputMessage.getOutputBody().write(data);
